eb4b2a962a2cdc61fa54818ed808c8e1f27375af
nelloodeluca <nelloodeluca@gmail.com>
2025-02-12 20:20:54 +0100

    frontend

---
+++ Home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Dashboard")
+st.write("Benvenuto nella Dashboard!")
+# Codice della dashboard
---
+++ ftmo.py
@@ -0,0 +1,5 @@
+import streamlit.components.v1 as components
+
+components.html(
+    "<a href='https://trader.ftmo.com/?affiliates=VOvEVWcPyiLwVjqQtItx'><img src='https://cdn.ftmo.com/affiliate-banner.728x90' alt='FTMO.com - For serious traders'/></a>"
+)
